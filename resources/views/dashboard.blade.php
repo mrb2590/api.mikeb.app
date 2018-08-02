73e1d84c157c4015ba59017c64edf4a0f670d2c0
Mike Buonomo <mrb2590@gmail.com>
2018-08-01 22:05:15 -0400

    Installed Passport

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,9 +14,13 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Welcome!
                 </div>
             </div>
+
+            <passport-clients></passport-clients>
+            <passport-authorized-clients></passport-authorized-clients>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
         </div>
     </div>
 </div>
