e08bd0d7511dc4fff25d81215111ac4da846d2c6
Mike Buonomo <mrb2590@gmail.com>
2018-08-15 22:00:13 -0400

    Added cors

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://files.mikeb.app'),
+    'url' => env('APP_URL', 'https://api.mikeb.app'),
 
     /*
     |--------------------------------------------------------------------------
---
+++ config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+   
+    'supportsCredentials' => false,
+    'allowedOrigins' => [
+        'dev.files.mikeb.app',
+        'files.mikeb.app',
+    ],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
